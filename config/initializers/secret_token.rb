a4ef949df215f169cbf08ee882bf829e2aaab821
Jeff Flannery <jflannery@teldargroup.com>
2012-06-25 08:45:37 -0600

    twitter rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResearch::Application.config.secret_token = '7d28cff43b716cfe08e92a4b0edf58930f8977f48ad80e05851389039c2310012c5ef6e71eda58582e39e6347f8cd68780e46b4b45e58c765a1b53576dde1786'
