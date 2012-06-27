53ec4fd21981f7f39738ad18e071329138e49daf
Jeff Flannery <jflannery@teldargroup.com>
2012-06-27 11:41:10 -0600

    Added twitter files from class

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,6 @@
 class SearchesController < ApplicationController
+  def show
+    searcher =  SimpleTwitter::Search.new
+    @results = searcher.search(params[:q])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TwitterResearch::Application.routes.draw do
   root(:to => 'searches#index')
+  resources(:searches)
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
