9a3be4f903ce12055bef988259b4b61b6c4181f6
Jeff Flannery <jflannery@teldargroup.com>
2012-07-23 11:44:30 -0600

    added save searches to twitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  respond_to(:html, :json)
   before_filter :authenticate_user!
   protect_from_forgery
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,10 @@
 class Tweet < ActiveRecord::Base
   # attr_accessible :title, :body
 
-  attr_accessible(:twitter_user, :tweeted_text, :tweeted_at, :user_id)
+  attr_accessible(:twitter_user, :tweeted_text, :tweeted_at)
   validates(:twitter_user, :presence => true)
   validates(:tweeted_text, :presence => true)
   validates(:tweeted_at, :presence => true)
-  validates(:user_id, :presence => true)
   belongs_to(:user)
   has_and_belongs_to_many(:categories)
   def suggested_categories
